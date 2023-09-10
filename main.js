03e0d2dae38e162a4107398b50bde48fa273349c
Avoy Baruah <113660114+Baruah123@users.noreply.github.com>
2023-09-10 21:53:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".new", {
+    strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
